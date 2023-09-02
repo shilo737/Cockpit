fba0931c419939dcf15969ccebec015b9e0357cb
shilo farajun <mr.shifu737@gmail.com>
2023-09-03 02:45:51 +0300

    finish

--- client/src/components/pages/PilotControl.jsx
+++ client/src/components/pages/PilotControl.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-import TextCockpit from "./TextCockpit ";
-import { useDispatch } from "react-redux";
-import { setADI, setAltitude, setHIS } from "../../redux/features/monitorSlice";
-import VisualCockpit from "./VisualCockpit";
+import TextCockpit from "./TextCockpit "; 
+import { useDispatch } from "react-redux"; 
+import { setADI, setAltitude, setHIS } from "../../redux/features/monitorSlice"; 
+import VisualCockpit from "./VisualCockpit"; 
 
+// Creating a connection to the Socket.IO 
 const socket = io.connect("http://localhost:3001");
 
+// Functional component for the pilot control interface
 const PilotControl = () => {
-  const dispatch = useDispatch();
-  const [openComponent, setOpenComponent] = useState(false);
+  const dispatch = useDispatch(); 
+  const [openComponent, setOpenComponent] = useState(false); // Managing component state with useState
 
   useEffect(() => {
+    // Setting up a Socket.IO event listener for "receive_message"
     socket.on("receive_message", (data) => {
+      // Dispatching Redux actions to update state with received data
       dispatch(setAltitude(data.altitude));
       dispatch(setHIS(data.HIS));
       dispatch(setADI(data.ADI));
     });
-  }, [dispatch]);
+  }, [dispatch]); // Running this effect whenever the dispatch function changes
 
   return (
     <div className="">
       <div className="flex items-center justify-center gap-5">
         <button
           className="btn bg-blue-600 text-white hover:bg-blue-900 mt-10"
-          onClick={() => setOpenComponent(false)}
+          onClick={() => setOpenComponent(false)} // Set openComponent to false when clicked
         >
           Visual
         </button>
+       
         <button
           className="btn bg-green-600 text-white hover:bg-green-900 mt-10"
           onClick={() => setOpenComponent(true)}
@@ -36,7 +41,7 @@ const PilotControl = () => {
           Text
         </button>
       </div>
-         {openComponent ? <TextCockpit /> : <VisualCockpit />}
+      {openComponent ? <TextCockpit /> : <VisualCockpit />}
     </div>
   );
 };
--- client/src/components/pages/TextCockpit .jsx
+++ client/src/components/pages/TextCockpit .jsx
@@ -3,7 +3,7 @@ import useMonitor from "../hooks/useMonitor";
 const TextCockpit = () => {
   const { ADI, HIS, altitude } = useMonitor();
   return (
-    <div className="flex flex-col justify-center items-center mt-20 opacity-90">
+    <div className="flex flex-col justify-center items-center  opacity-90">
       <div className="w-[700px] p-10 bg-slate-500  rounded-2xl text-white font-semibold text-[2em]">
         <div className="mb-4">
           <p>Altitude : {altitude}</p>
